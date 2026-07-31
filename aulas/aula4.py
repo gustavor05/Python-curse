b4a3db2e17b87d4b891690bc05c81914da6b52d6
Gustavo Ramos <gustavorcavalcante85@gmail.com>
2026-07-30 23:28:45 -0300

    Adiciona aula 4

---
+++ aulas/aula4.py
@@ -0,0 +1,17 @@
+"""
+O que é int e e float?
+
+int = numero inteiro positivo ou negativo sem pontuação
+
+float = numero com ponto flutuante
+
+"""
+
+#Int
+print(6)
+print(-7, 5)
+
+#float
+print(1.1, 10.11, sep='-')
+
+print( type('Gustavo')      )
